54e3ce393157c0325973ebf33273dba69c41837a
kobayashi-blitz <kobaayshi@blitz.vc>
2025-05-29 18:13:11 +0900

    Initial Commit

---
+++ MapboxDemo2-iOS/MapboxDemo2_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxDemo2_iOSApp.swift
+//  MapboxDemo2-iOS
+//
+//  Created by Shinya Kobayashi on 2025/05/29.
+//
+
+import SwiftUI
+
+@main
+struct MapboxDemo2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapboxDemo2-iOSTests/MapboxDemo2_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxDemo2_iOSTests.swift
+//  MapboxDemo2-iOSTests
+//
+//  Created by Shinya Kobayashi on 2025/05/29.
+//
+
+import Testing
+@testable import MapboxDemo2_iOS
+
+struct MapboxDemo2_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
